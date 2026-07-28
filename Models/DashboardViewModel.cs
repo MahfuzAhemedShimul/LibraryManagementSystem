0e584ec159177b13598fa859e611b8dbaf258ff4
Deep Learner X <22109104@iubat.edu>
2026-07-28 23:21:04 +0600

    update

---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryManagementSystem.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalBooks { get; set; }
+        public int ActiveBorrowers { get; set; }
+        public int OverdueFinesCount { get; set; }
+        public decimal TotalRevenueCollected { get; set; }
+
+        // Data for Category Pie Chart
+        public List<string> CategoryLabels { get; set; } = new();
+        public List<int> CategoryCounts { get; set; } = new();
+
+        // Data for Monthly Borrowings Line Graph
+        public List<string> MonthLabels { get; set; } = new();
+        public List<int> MonthlyBorrowingCounts { get; set; } = new();
+    }
+}
